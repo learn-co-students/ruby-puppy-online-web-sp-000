e32c41be3137d576c3db1276e11134a63ab4b838
Sung Bae <singsang2@Sungs-iMac.local>
2018-10-30 15:23:51 -0400

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,21 @@
+class Dog
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def name
+    @name
+  end
+
+  def self.all
+    @@all.each {|puppy| puts puppy.name}
+  end
+
+  def self.clear_all
+    @@all.clear
+  end
+
+end
