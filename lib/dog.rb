0dc7ac02094852868f0e4f0a82c714ec49f36837
Alexander Baker <alex.baker685@gmail.com>
2019-01-06 22:32:05 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,19 +1,21 @@
 require 'pry'
 
 class Dog
+
   @@all = []
-  @@all_names = []
+  # @@all_names = []
   attr_accessor :name
 
   def initialize(name)
     @name = name
     @@all << self
-    @@all_names << name
+    # @@all_names << name
   end
 
   def self.all
-    @@all_names.uniq!
-    @@all_names.each {|name| puts "#{name}"}
+    # @@all_names.uniq!
+    # @@all_names.each {|name| puts "#{name}"}
+    puts @@all.map{ |dog| dog.name }
   end
 
   def self.clear_all
