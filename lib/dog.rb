fa2ff3e0bb3c82c45bf78b32f769e5f9ebe41887
Diana Badas <diana.badas@gmail.com>
2019-01-18 15:05:43 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,5 +1,5 @@
 class Dog
- attr_accessor :name
+  attr_accessor :name
   @@all = []
 
   def initialize(name)
@@ -7,8 +7,17 @@ class Dog
     @@all << self
   end
 
+  #def name=(the_name)
+    #@name = the_name
+  #end
+
+  #def name
+    #@name
+  #end
+
   def self.all
-    @@all.each {|dog| puts "#{dog.instance_variable_get(:@name)}"}
+    #@@all.each {|dog| puts "#{dog.instance_variable_get(:@name)}"}
+    @@all.each {|dog| puts "#{dog.name}"}
   end
 
   def self.clear_all
