1162b04720f79290af771b1379fcdd2c6cc3480d
kathinka martinsen <kathinkamartinsen@gmail.com>
2019-01-30 20:12:16 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -6,12 +6,10 @@ class Dog
 def initialize(name)
   @name = name
   @@all << self
-  @@names << name
-
 end
 
 def self.all
-@@names.uniq.each {|name| puts "#{name}"} 
+puts @@all.map{|dog| dog.name}
 end
 
 
