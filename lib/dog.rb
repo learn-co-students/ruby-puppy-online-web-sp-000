f1fe354708cf42da144c857ba72bfdcbf8eaba03
Lauren McCoy <lmccoy8911@gmail.com>
2019-04-02 21:23:25 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,24 @@
+class Dog 
+  
+  @@all = [] 
+  
+  def initialize(name)
+    @name = name 
+    @@all << self 
+  end 
+  
+  def self.all 
+    @@all.each do |dog|
+       print "#{@name}" 
+    end 
+  end
+  
+  def self.clear_all 
+    @@all.clear 
+  end 
+  
+  def self.name 
+    @name.upcase 
+  end 
+  
+end 
