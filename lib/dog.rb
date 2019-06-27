27859692f401c795cd39c5e71a90c7768780acdc
Peter Grant <jgrantps@hotmail.com>
2019-06-27 18:24:18 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,32 @@
-# Add your code here
+class Dog
+  attr_accessor :name
+
+  @@all = []
+  @@names = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+    @@names << self.name
+  end
+
+  def self.print_all
+  #  printout =  @@names.map {|i| i.count < 4 }
+  puts @@names.uniq
+  #  puts printout
+  end
+
+  def self.clear_all
+    @@all = []
+  end
+
+  def self.all
+    @@all
+  end
+
+end
+
+Dog.new("Pluto")
+Dog.new("Fido")
+Dog.new("Maddy")
+Dog.print_all
