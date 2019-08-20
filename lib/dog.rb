cc9ec8837c09180a681bc44371368d0a78c1583b
David Strauch <dmaster18@gmail.com>
2019-08-20 00:05:52 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,38 @@
-# Add your code here
+class Dog
+  @@all = []
+    
+    def save
+      @@all << self
+    end
+
+  def initialize(name)
+    @name = name
+    save
+  end
+  
+  def name
+    @name
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def self.print_all
+    @@all.map do |dog|
+      puts dog.name
+    end
+  end
+  
+  def self.clear_all
+    @@all.clear
+  end
+end
+
+clifford = Dog.new("Clifford")
+odie = Dog.new("Odie")
+fido = Dog.new("Fido")
+cujo = Dog.new("Cujo")
+Dog.print_all
+Dog.clear_all
+Dog.print_all
