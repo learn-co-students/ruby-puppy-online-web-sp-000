84c2fb8265ed6051c472efa28244d201277ba240
Blythe Pollard <blytheepollard@gmail.com>
2019-10-01 19:48:30 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,32 @@
-# Add your code here
+require 'pry'
+class Dog
+  @@all = []
+  attr_accessor :name
+  
+  def initialize(name)
+    @name = name
+    save
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def self.print_all
+    binding.pry
+   new_array = []
+    @@all.each do |dogs|
+      new_array << dogs
+      return new_array
+    end
+  end
+  
+  def save
+    @@all << self
+  end
+  
+  def self.clear_all
+    @@all.clear
+  end
+  
+end
