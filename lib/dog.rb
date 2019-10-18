26877477bb3de25518b71f235504e79f52958305
Ronda D Cowen <cowenhome5@gmail.com>
2019-10-18 15:22:48 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,29 @@
-# Add your code here
+class Dog
+
+  @@all = []
+  attr_accessor :name
+  attr_reader :save
+
+  def initialize(name)
+    @name = name
+    @save = save
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+ def self.clear_all
+   @@all.clear
+ end
+
+ def self.print_all
+   @@all.map do |dog_object|
+      puts dog_object.name
+    end
+ end
+end
