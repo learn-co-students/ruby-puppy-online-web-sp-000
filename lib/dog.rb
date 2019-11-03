96c0d47d3a48136da897ed9540dd2be1a41a8b69
Ebisiemen Ajayi <ebisiemen@gmail.com>
2019-11-03 21:24:56 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,33 @@
-# Add your code here
+require 'pry'
+class Dog 
+  
+  attr_accessor :name 
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name 
+    @@all << self 
+    save
+  end   
+  
+  def self.all 
+    @@all 
+  end   
+  
+  def self.clear_all
+    @@all.clear 
+  end   
+  
+  def self.print_all 
+    puts @@all.map{ |dog| dog.name } 
+  end   
+  
+  def save 
+    # binding.pry 
+   if !@@all.include?(self) 
+    # else 
+      @@all << self 
+    end   
+  end   
+end   
