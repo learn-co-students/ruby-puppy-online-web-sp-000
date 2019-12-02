21e7be8528efce4c84c20c4e79e55136e46080df
Jason J Cruz <jaycru06@yahoo.com>
2019-12-02 23:34:35 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,34 @@
-# Add your code here
+class Dog 
+  
+  @@all = [] 
+   
+  attr_accessor :dog_name 
+   
+  def initialize(dog_name)
+    @dog_name = dog_name 
+    save 
+  end 
+   
+  def self.all
+    @@all 
+  end 
+  
+  def self.print_all 
+    print_names = []
+    print_names = self.all.map(&:dog_name)
+    puts print_names.join("\n")
+  end
+  
+  def self.clear_all 
+    @@all.clear 
+  end 
+  
+  def save 
+    @@all.push(self)  
+  end 
+  
+  def name
+    self.dog_name
+  end
+   
+end 
