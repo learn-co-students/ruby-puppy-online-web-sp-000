8083a41b416153c6a9147ef0c5b7863965e00836
Sweet Van Loan <sweet@sweetvanloan.com>
2020-01-03 00:47:11 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,36 @@
 # Add your code here
+
+class Dog
+  
+  @@all = [ ]
+  attr_accessor :dog, :name
+  
+  def initialize(name)
+    @name = name
+    save
+  end 
+  
+  def self.all
+    @@all
+  end
+
+  def self.name 
+    @name 
+  end 
+
+  
+  def self.print_all    
+    puts @@all.map{ |dog| dog.name }
+  end 
+  
+  def self.clear_all
+    @@all.clear
+  end
+  
+  
+  def save
+    @@all <<  self # dog
+  end 
+  
+  
+end
