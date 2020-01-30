eaeba267b81f89bf1ee48a6a5e7efb380f49a914
Doak Covington <doakcovington@protonmail.com>
2020-01-30 04:17:53 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,30 @@
 # Add your code here
+class Dog
+  attr_accessor :name
+
+  @@all = []#stores each instances of class
+
+  def initialize(name)
+    @name = name
+    save
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.print_all
+    @@all.each do |dog|
+      puts dog.name
+    end
+  end
+
+  def self.clear_all
+    @@all.clear
+  end
+
+  def save
+    @@all << self#shovels instance to @@all array when instantiated
+  end
+
+end
