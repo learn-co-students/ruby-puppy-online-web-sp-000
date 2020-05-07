d17080ed343d333e0890981db043e735ae4f9396
NicholasR77 <nl7robbins@icloud.com>
2020-05-06 18:43:13 -0700

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,33 @@
 # Add your code here
+#fido = Dog.new("Fido")
+#sam = Dog.new("Sam")
+
+class Dog
+    @@all = []
+    attr_reader :name
+
+    def initialize(name)
+        @name = name
+        save
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.print_all
+        @@all.each do |element|
+            if (element != nil)
+                puts element.name
+            end
+        end                
+    end
+
+    def save
+        @@all.push(self)
+    end
+    
+    def self.clear_all
+        @@all.clear
+    end
+end
