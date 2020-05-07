d82dd50291af8c4846ecef1dba869f3318bf9809
Matt Im <matthew.seiim@gmail.com>
2020-05-07 20:38:23 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,30 @@
-# Add your code here
+class Dog 
+  
+attr_accessor :name 
+
+@@all = []
+
+  def initialize(name)
+    @name = name
+    self.save
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def self.print_all #iterates over all of the indiv. dogs stored and puts out their name
+    @@all.each do |dog|
+      puts dog.name
+    end
+  end
+  
+  def save
+    @@all << self
+  end
+  
+  def self.clear_all
+    @@all.clear
+  end
+
+end
