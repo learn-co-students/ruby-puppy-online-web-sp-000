3ee8ec076e54968c499e715256e3cc5c5a0c9a7c
Seth Near <seth.j.near@gmail.com>
2020-06-23 14:14:20 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,30 @@
-# Add your code here
+class Dog
+
+@@all = []
+
+attr_reader :name
+
+def initialize(name)
+  @name = name
+  self.save
+end
+
+def self.all
+  return @@all
+end
+
+def self.clear_all
+  @@all.clear
+end
+
+def self.print_all
+  @@all.each {|dog|
+    puts "#{dog.name}"
+  }
+end
+
+def save
+  @@all << self
+end
+
+end
