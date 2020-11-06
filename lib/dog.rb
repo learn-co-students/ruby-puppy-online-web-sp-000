0b08503f7b78bf741d02155b2575cc5549d8a1d9
VandelayUtd <drewhadley.guitar@gmail.com>
2020-11-06 10:12:27 -0500

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,29 @@
 # Add your code here
+require 'pry'
+class Dog
+    attr_reader :name
+    @@all = []
+    
+    def initialize(name)
+        @name = name
+        save
+    end
+
+    def self.all 
+        @@all    
+    end
+
+    def self.print_all
+        @@all.each do |instance|
+               puts instance.name
+        end
+    end
+
+    def self.clear_all
+        @@all = []
+    end
+
+    def save
+        @@all << self
+    end
+end
