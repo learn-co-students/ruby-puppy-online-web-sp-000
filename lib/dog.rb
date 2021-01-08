f4a7cf88a919dc9d34842ebfbef1cb9e39ad7372
HELEN HARMAN <hlharman@bellsouth.net>
2021-01-08 19:10:23 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,4 +1,4 @@
-require 'byebug'
+require 'pry'
 
 class Dog 
  @@all = []
@@ -7,21 +7,41 @@ class Dog
   def initialize(name = "Snoopy")
     @name = name 
     @@all << self 
+    #self.save
+    #self.all 
+    #save
+    self.save 
   end  
     
 def self.all
   @@all
 end 
 
-def self.clear_all()  
+def self.clear_all
 a = @@all.clear 
 end 
 
+# def self.clear_all
+#     @@all = [] 
+#   end
+
 def self.print_all
-  @@all.each do |name|
-    byebug
-    puts name 
+  @@all.each do |puppy|
+    puts puppy.name 
 end 
 end
-end
-  
+
+# def save
+# @@all.each do |puppy|
+# @@all << puppy 
+
+
+def save
+    @@all << self
+    return self.name 
+  end
+
+  def name
+    @name
+  end
+end 
