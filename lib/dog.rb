d789d2195be37f78c0531f5fb5569687c7c1ac0e
Mike Nanto <mike@thecowboymechanic.com>
2021-02-01 07:45:14 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,28 @@
 # Add your code here
+class Dog 
+attr_accessor :name
+
+@@all = []
+
+def self.all 
+  @@all 
+end
+
+def self.print_all
+ @@all.each { |n| puts n.name}
+end
+  
+def initialize(name)
+  @name = name 
+  self.save
+end 
+
+def save
+  @@all << self
+end
+
+def self.clear_all
+  @@all.clear 
+end
+
+end
