5a71cb3c418e865446d8d4f7b918c455ceb72261
Jacob W Jones <jacobwadejones76@gmail.com>
2021-03-08 21:46:58 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,30 @@
-# Add your code here
+class Dog
+  
+  attr_accessor :name
+  
+  @@all = []
+  @@names = []
+  
+  def initialize(name)
+    @name = name
+    save
+    @@names << name
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def self.clear_all
+    @@all = []
+  end
+  
+  def self.print_all
+    puts @@names.uniq - ["Snoopy"]
+  end
+  
+  def save
+    @@all << self
+  end
+
+end
