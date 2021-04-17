fdc828a74e89ac365cae63b1e0da43b503f68a89
Christopher Black <christopher.black.2209@googlemail.com>
2021-04-17 11:02:05 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,21 @@
-# Add your code here
+class Dog
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @@all << self 
+  end
+  
+  def self.all
+    @@all  
+  end
+  
+  def self.clear_all
+    @@all.clear
+  end
+  
+  def self.print_all 
+  @@all.each {|dog| puts dog} 
+  end 
+end 
